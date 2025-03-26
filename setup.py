72421b6cc279ed78a5c0b1542221f9a34106d126
messoem <57996364+messoem@users.noreply.github.com>
2025-03-26 12:48:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tms-nets",
+    name="tms_nets",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["numpy", "seaborn", "pandas", "matplotlib", "galois"],
