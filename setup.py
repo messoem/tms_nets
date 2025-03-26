9d258da62bcb37fb16a266f6ee7a34ed067d8fcd
messoem <57996364+messoem@users.noreply.github.com>
2025-03-26 13:13:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="tms_nets",
     version="0.1.0",
+    author="Arsenii Sychev",
+    description="Niederreiter algo",
+    url="https://github.com/messoem/tms_nets",
     packages=find_packages(),
     install_requires=["numpy", "seaborn", "pandas", "matplotlib", "galois"],
+    
 )
