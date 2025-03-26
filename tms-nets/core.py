7fac513f6491b8f231e2890c3ff611abc40bf2e4
messoem <57996364+messoem@users.noreply.github.com>
2025-03-26 12:29:52 +0300

    Update core.py

--- tms-nets/core.py
+++ tms-nets/core.py
@@ -1,4 +1,23 @@
 import tms-nets.utils as utils
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_plot(p):
+    if (p.shape)[1] == 2:
+        df = pd.DataFrame(p, columns=["x", "y"])
+        #print(df)
+        sns.scatterplot(df, x="x", y="y").plot()
+    elif (p.shape)[1] == 3:
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection='3d')
+        df = pd.DataFrame(p, columns=["x", "y", "z"])
+        ax.scatter(df['x'], df['y'], df['z'])
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('Z-axis')
+        plt.title('3D Scatter Plot with Seaborn')
+        plt.show()
 
 def get_points(b, t, m, s):
     G = utils.generate_generator_matrices(b, t, m, s)
