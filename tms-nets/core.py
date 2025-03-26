b467d38e85594e3d88c33a5c826ff4d27ffd62ba
messoem <57996364+messoem@users.noreply.github.com>
2025-03-26 12:09:13 +0300

    Create core.py

---
+++ tms-nets/core.py
@@ -0,0 +1,14 @@
+import tms-nets.utils
+
+def get_points(b, t, m, s):
+    G = utils.generate_generator_matrices(b, t, m, s)
+    points = np.ones((b**m, s))
+    for i in range(s):
+        for n in range(0, b**m):
+            #print(G[i]@ utils.vecbm(b, m, n))
+            tmp = G[i]@ utils.vecbm(b, m, n)
+            for j in range(len(tmp)):
+                tmp[j] = tmp[j] % b
+            points[n][i] = utils.rnum(b, tmp)*b**(-m)
+    return points
+
