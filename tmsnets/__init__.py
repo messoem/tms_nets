34cc4bdd3b5d54800426e8d7e127d11c7e30350a
Arsenii Sychev <57996364+messoem@users.noreply.github.com>
2025-05-25 15:46:47 +0300

    Update __init__.py

--- tmsnets/__init__.py
+++ tmsnets/__init__.py
@@ -1,4 +1,4 @@
-from .core import get_points_opt, get_plot, generate_generator_matrices
+from .core import get_points_opt, get_plot, generate_generator_matrices, h_net_rosenbloom_tsfasman
 
 
-__all__ = ["get_points_opt", "get_plot", "generate_generator_matrices"]
+__all__ = ["get_points_opt", "get_plot", "generate_generator_matrices", "h_net_rosenbloom_tsfasman"]
