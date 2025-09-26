77148e5a77602c691e17bfd29791772175d3a362
Arsenii Sychev <57996364+messoem@users.noreply.github.com>
2025-09-26 09:42:45 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,22 @@
+import os
+import sys
+# Эта строка указывает Sphinx, где искать ваш код (в корне репозитория)
+sys.path.insert(0, os.path.abspath('../../'))
 
+# -- Project information -----------------------------------------------------
+project = 'tms_nets'
+copyright = '2025, Arsenii Sychev'
+author = 'Arsenii Sychev'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',  # Автоматически импортирует документацию из docstrings
+    'sphinx.ext.viewcode', # Добавляет ссылки на исходный код
+    'sphinx.ext.napoleon', # Понимает docstrings в стиле Google и NumPy
+]
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
